92953da134f27cf4349419fb193fb000abc25333
rollcloud <rollcloud@gmail.com>
2023-12-01 20:52:46 +0000

    feat: add motor control

--- hardware.py
+++ hardware.py
@@ -2,9 +2,15 @@ from time import sleep_ms
 
 from machine import ADC, Pin
 
+from lib.SimplyRobotics import SimplePWMMotor
+
+FORWARD = "f"
+REVERSE = "r"
+
 CONVERSION_FACTOR = 3.3 / (65535)
 
 led = Pin("LED", Pin.OUT)
+motor = None
 
 
 def flash_led(t: float = 0.12, n: int = 1):
@@ -28,3 +34,25 @@ def get_internal_temperature():
     # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree.
     temperature = 27 - (reading - 0.706) / 0.001721
     return temperature
+
+
+def init_motor():
+    """Initialise a SimplePWMMotor."""
+    global motor
+    motor = SimplePWMMotor(2, 5, 100)
+
+
+def motor_on(direction, percentage_vmax: float):
+    """A wrapper for SimplePWMMotor.on"""
+    global motor
+    if motor is None:
+        raise Exception("Motor not initialised")
+    motor.on(direction, percentage_vmax)
+
+
+def motor_off():
+    """A wrapper for SimplePWMMotor.off"""
+    global motor
+    if motor is None:
+        raise Exception("Motor not initialised")
+    motor.off()
