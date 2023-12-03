48fbf21d057fee75e741bcc310b4e7a47bc7b557
rollcloud <rollcloud@gmail.com>
2023-12-03 19:03:40 +0000

    feat: upgrade to micro server

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
-from phew import get_ip_address, server
+from microdot import Microdot, send_file
+from phew import get_ip_address
 from phew.logging import LOG_ALL, disable_logging_types
-from phew.template import render_template
 
-from layout import RelativeDirection
 import hardware
-import wifi
 import throttle
+import wifi
+from layout import RelativeDirection
+
+ONE_HOUR_IN_SECONDS = 60 * 60
+ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24
 
 disable_logging_types(LOG_ALL)
 flash_led = hardware.flash_led
@@ -14,48 +17,44 @@ wifi.connect()
 flash_led(n=2)  # show that wifi connection was successful
 
 
+app = Microdot()
+
+
 def title(string: str):
     return string[0].upper() + string[1:]
 
 
-@server.route("/favicon.ico", methods=["GET"])
+@app.before_request
+def before(request):
+    flash_led(t=0.01)
+
+
+@app.get("/favicon.ico")
 def favicon(request):
-    pass
+    return send_file("/static/favicon.ico", max_age=ONE_HOUR_IN_SECONDS)
 
 
-@server.route("/", methods=["GET"])
-def hello(request):
-    flash_led(t=0.01)
-    return render_template("templates/throttle.html")
+@app.route("/static/<path:path>")
+def static(request, path):
+    if ".." in path:
+        # directory traversal is not allowed
+        return "Not found", 404
+    return send_file("static/" + path, max_age=ONE_DAY_IN_SECONDS)
 
 
-@server.route("/world", methods=["GET"])
-def world(request):
-    flash_led(t=0.01)
-    return render_template(
-        "templates/jumbotron-page.html",
-        title="Hello Wide World",
-        heading="Welcome to the",
-        sub_heading="World Wide Web",
-    )
+@app.get("/")
+def index(request):
+    return send_file("templates/throttle.html")
 
 
-@server.route("/stop", methods=["GET"])
+@app.get("/stop")
 def stop(request):
-    flash_led(t=0.01)
-
     throttle.stop()
-    return render_template(
-        "templates/jumbotron-page.html",
-        title="Stopping",
-        heading="Stopping",
-    )
+    return "stopping"
 
 
-@server.route("/move/<dir>", methods=["GET"])
+@app.get("/move/<dir>")
 def move(request, dir):
-    flash_led(t=0.01)
-
     acceptable_dirs = ["forward", "reverse", "left", "right"]
 
     if dir == "left":
@@ -64,40 +63,24 @@ def move(request, dir):
         dir = "reverse"
 
     if dir not in acceptable_dirs:
-        return render_template(
-            "templates/jumbotron-page.html",
-            title="400 Bad Request",
-            heading="400 Bad Request",
-            sub_heading=f"Direction '{dir}' is no one of {', '.join(acceptable_dirs)}",
-        )
+        return f"Direction '{dir}' is no one of {', '.join(acceptable_dirs)}", 400
 
     direction = RelativeDirection.FORWARD if dir == "forward" else RelativeDirection.REVERSE
     throttle.move(direction)
-    return render_template(
-        "templates/jumbotron-page.html",
-        title=f"Moving {title(dir)}",
-        heading=f"Move {title(dir)}",
-    )
+    return f"moving {title(dir)}"
 
 
-@server.catchall()
-def catchall(request):
-    flash_led(t=0.01)
-    return render_template(
-        "templates/jumbotron-page.html",
-        title="404 Not Found",
-        heading="404 Not Found",
-        sub_heading="This is not the page you're looking for",
-    )
+@app.errorhandler(404)
+def not_found(request):
+    return "This is not the page you're looking for", 404
 
 
 try:
     address = get_ip_address()
-    print(f"Running server on http://{address}")
-    server.run()
+    print(f"Running app on http://{address}")
+    app.run(port=80)
 except KeyboardInterrupt:
-    server.stop()
-    server.close()
+    app.shutdown()
 except Exception as err:
     flash_led(t=1, n=5)
     import sys
