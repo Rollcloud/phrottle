2f4f48db35e2fdbf4b1988128f26416a79edab4f
rollcloud <rollcloud@gmail.com>
2024-01-03 12:22:13 +0000

    wip: still crashing after a couple seconds
    seems to be https://github.com/orgs/micropython/discussions/10700

--- src/rc_detector.py
+++ src/rc_detector.py
@@ -1,18 +1,14 @@
 from machine import Pin, Timer
 import utime
 
-LOW_FREQUENCY_PERIOD_MS = 100
-
-up_time = 0  # ms
-
 
 class RcDetector:
     def calibrate(self, *args):
         # args provided for callback use
         try:
-            self.calibration = self.sense_time
+            self.calibration = self._sense_time
         except AttributeError:
-            #'RcDetector' object has no attribute 'sense_time'
+            #'RcDetector' object has no attribute '_sense_time'
             pass
 
     def _filter(self, x) -> float:
@@ -32,8 +28,8 @@ class RcDetector:
 
     def _stop_read(self, _pin):
         self._stop_time = utime.ticks_us()
-        self.sense_time = utime.ticks_diff(self._stop_time, self._start_time)
-        self.sense_time = self._filter(self.sense_time)
+        self._sense_time = utime.ticks_diff(self._stop_time, self._start_time)
+        self._sense_time = self._filter(self._sense_time)
         self.read_taken = True
 
     def __init__(self, pin_number, threshold_us=30, filter_alpha=0.0) -> None:
@@ -43,42 +39,52 @@ class RcDetector:
         self._sensor = Pin(pin_number, Pin.IN)
 
         self.threshold_us = threshold_us
+        self.read_taken = False
         self.calibration = 0  # us
         # Measure the time for the voltage to decay by waiting for the I/O line to go low.
         self._sensor.irq(handler=self._stop_read, trigger=Pin.IRQ_FALLING)
 
     def perform_read(self) -> None:
         """Take a new reading."""
+        print("(", end="")
         # Set the I/O line to an output and drive it high.
         self._sensor.init(mode=Pin.OUT, value=1)
         # Allow at least 10 μs for the sensor output to rise.
+        print("|", end="")
         utime.sleep_us(15)
+        print("/", end="")
         # Make the I/O line an input (high impedance).
         self._sensor.init(mode=Pin.IN)
         self._start_time = utime.ticks_us()
         self.read_taken = False
+        print(")", end="")
+
+    def value(self) -> float:
+        """Return the detector value in us or zero if no value present."""
+        try:
+            return self._sense_time
+        except AttributeError:
+            return 0.0
 
     def is_present(self) -> bool:
         """Return whether an object is present."""
         try:
-            present = self.sense_time <= self.calibration - self.threshold_us
+            present = self._sense_time <= self.calibration - self.threshold_us
             return present
         except AttributeError:
-            #'RcDetector' object has no attribute 'sense_time'
+            #'RcDetector' object has no attribute '_sense_time'
             return False
 
 
 if __name__ == "__main__":
+    LOW_FREQUENCY_PERIOD_MS = 100
+
+    up_time = 0  # ms
+    is_lf_loop_ready = False
+    is_sensors_loop_ready = False
+
     led = Pin("LED", Pin.OUT)
     detector = RcDetector(26, threshold_us=5, filter_alpha=0.05)
-
-    def read_sensors(_timer):
-        """Take a sensor reading and prep for the next reading."""
-        global detector
-        # present = detector.is_present()
-        # led.value(present)
-        detector.perform_read()
-        print("-", end="")
 
     def pos(value, scale, zero):
         pos = (value - zero) * scale
@@ -92,62 +98,73 @@ if __name__ == "__main__":
             try:
                 graph[idx] = "|"
             except IndexError as err:
-                print(f"position {idx} is not <= {width}")
-        graph[value_pos] = "*"
+                print(f"Ruler position {idx} is not <= {width}")
+                raise err
+        try:
+            graph[value_pos] = "*"
+        except IndexError as err:
+            print(f"Value position {value_pos} is not <= {width}")
+            raise err
         return "".join(graph)
 
     def print_feedback():
         global detector
         present = detector.is_present()
-        sense_time = detector.sense_time
         icon = "|" if present else "-" if detector.read_taken else "X"
         graph = draw_graph(
-            detector.sense_time,
-            scale=0.05,
+            detector.value(),
+            scale=0.01,
             rulers=[detector.calibration - detector.threshold_us],
         )
-        print(f"{detector.calibration/1000:.3f} {sense_time/1000: 4.3f} ms {icon}{graph}")
+        print(f"{detector.calibration/1000:.3f} {detector.value()/1000: 4.3f} ms {icon}{graph}")
 
     def low_frequency_callback(_timer):
-        global detector, up_time
+        global is_lf_loop_ready, up_time
         up_time += LOW_FREQUENCY_PERIOD_MS
+        is_lf_loop_ready = True
 
-        print("*", end="")
+    def sensors_callback(_timer):
+        global is_sensors_loop_ready
+        is_sensors_loop_ready = True
+
+    sensor_timer = Timer()
+    sensor_timer.init(mode=Timer.PERIODIC, period=200, callback=sensors_callback)
+
+    # lf_timer = Timer()
+    # lf_timer.init(
+    #     mode=Timer.PERIODIC, period=LOW_FREQUENCY_PERIOD_MS, callback=low_frequency_callback
+    # )
+
+    def sensors_loop():
+        global detector
+        # present = detector.is_present()
+        # led.value(present)
+        detector.perform_read()
+
+    def low_frequency_loop():
+        global up_time, detector
 
         if up_time == 2000:  # ms
             detector.calibrate()
 
-    sensor_timer = Timer()
-    sensor_timer.init(mode=Timer.PERIODIC, period=50, callback=read_sensors)
-
-    lf_timer = Timer()
-    lf_timer.init(
-        mode=Timer.PERIODIC, period=LOW_FREQUENCY_PERIOD_MS, callback=low_frequency_callback
-    )
-
-    # while True:
-    #     detector.perform_read()
-    #     utime.sleep_ms(5)
-    #     present = detector.is_present()
-    #     led.value(present)
-    #     sense_time = detector.sense_time
-    #     icon = "|" if present else "-" if detector.read_taken else "X"
-    #     graph = draw_graph(
-    #         detector.sense_time,
-    #         scale=0.05,
-    #         rulers=[detector.calibration - detector.threshold_us],
-    #     )
-    #     print(f"{detector.calibration/1000:.3f} {sense_time/1000: 4.3f} ms {icon}{graph}")
-
-    utime.sleep_ms(500)
+        print_feedback()
 
     try:
         while True:
-            utime.sleep_ms(100)
-            # print_feedback()
-            print(".", end="")
+            if is_sensors_loop_ready:
+                print("{", end="")
+                sensors_loop()
+                print("}", end="")
+                is_sensors_loop_ready = False
+
+            if is_lf_loop_ready:
+                low_frequency_loop()
+                is_lf_loop_ready = False
+
+            # print(".", end="")
+
     except KeyboardInterrupt:
         print("Keyboard exit detected")
 
     sensor_timer.deinit()
-    lf_timer.deinit()
+    # lf_timer.deinit()
