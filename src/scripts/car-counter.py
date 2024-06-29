3bd9b11df96bcc099b14015f4f0417e0adbe1b25
rollcloud <rollcloud@gmail.com>
2024-06-29 21:17:34 +0100

    feat (scripts): add toggling and robust detectors

--- src/scripts/car-counter.py
+++ src/scripts/car-counter.py
@@ -1,6 +1,16 @@
 import utime
-from detectors import AnalogueDetector, DetectorEvent, AGCDetector, SchmittDetector
+from detectors import (
+    AGCConverter,
+    AnalogueDetector,
+    BehaviourEvent,
+    DebounceBehaviour,
+    DigitalDetector,
+    EventOnChangeBehaviour,
+    SchmittConverter,
+    SimpleThresholdConverter,
+)
 from hardware import led
+from layout import Point
 
 COUNT_THRESHOLD = 50
 CLEAR_THRESHOLD = 70
@@ -13,11 +23,26 @@ REFLECTOR_LENGTH = 5  # mm
 min_trigger_interval = int(MIN_WAGON_LENGTH / MAX_SPEED * 1000)  # 83 ms
 min_trigger_duration = int(REFLECTOR_LENGTH / MAX_SPEED * 1000)  # 17 ms
 
-detector = AnalogueDetector(28)
-agc_detector = AGCDetector(detector, base_threshold=15, gain=0.05)
-schmitt_detector = SchmittDetector(
-    detector, trigger_threshold=COUNT_THRESHOLD, release_threshold=CLEAR_THRESHOLD
+point = Point(motor_number=1, id="Point")
+
+# Touch sensor
+touch_detector = DigitalDetector(0)
+touch_converter = SimpleThresholdConverter(touch_detector, threshold=1)
+touch_debouncer = DebounceBehaviour(touch_converter, debounce_time_ms=1000)
+touch_behaviour = EventOnChangeBehaviour(touch_debouncer)
+
+# Wagon presence sensor
+wagon_detector = AnalogueDetector(28)
+wagon_converter = AGCConverter(wagon_detector, base_threshold=15, gain=0.05)
+wagon_debouncer = DebounceBehaviour(wagon_converter, debounce_time_ms=100)
+
+# Wagon counter
+wagon_counter = SchmittConverter(
+    wagon_detector, trigger_threshold=COUNT_THRESHOLD, release_threshold=CLEAR_THRESHOLD
 )
+wagon_count_debouncer = DebounceBehaviour(wagon_counter, debounce_time_ms=min_trigger_interval)
+wagon_count_events = EventOnChangeBehaviour(wagon_count_debouncer)
+
 counts = 0
 
 
@@ -25,14 +50,24 @@ if __name__ == "__main__":
     led.off()
 
     while True:
-        led.value(agc_detector.is_present())
-        # print(agc_detector.value(), agc_detector.base)
+        # Light LED when wagon is over sensor
+        led.value(wagon_debouncer.is_present())
 
-        event = schmitt_detector.is_present(time_hysteresis=min_trigger_interval)
-        if event == DetectorEvent.TRIGGER:
+        # Increment counter when wagon is detected
+        event = wagon_count_events.check_event()
+        if event == BehaviourEvent.TRIGGER:
             counts += 1
             print(counts, end="")
-        elif event == DetectorEvent.RELEASE:
+        elif event == BehaviourEvent.RELEASE:
             print(".")
+
+        # Toggle point when touched
+        touch_event = touch_behaviour.check_event()
+        if touch_event == BehaviourEvent.TRIGGER:
+            # Do not toggle if wagon is present
+            if wagon_debouncer.is_present():
+                print("Wagon present, cannot toggle point.")
+            else:
+                point.toggle()
 
         utime.sleep_ms(min_trigger_duration)
