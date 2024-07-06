dc7b9eb1b3f7d18e8bbce0ffb95d63cd70b9d65f
rollcloud <rollcloud@gmail.com>
2024-07-06 21:44:22 +0100

    feat (setup): compared modified times before syncing files

--- src/setup/sync.py
+++ src/setup/sync.py
@@ -5,10 +5,18 @@ import sys
 from pathlib import Path
 
 import mpremote
+import yaml
 
 SOURCE = Path("src/rp2/")
 DEST = Path(":")
 DEFAULT_SEARCH = "*"
+
+SETTINGS = Path("src/setup/sync.yaml")
+
+settings = {}
+if SETTINGS.exists():
+    with SETTINGS.open("r") as f:
+        settings = yaml.safe_load(f)
 
 
 search = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_SEARCH
@@ -30,6 +38,9 @@ def mpremote_fs_cp(source: Path, dest: Path):
     else:
         # print(f"Copying {source_file} to {dest_file}")
         subprocess.run(f"mpremote fs cp {source} {dest}", check=True, shell=True)
+        if "files" not in settings:
+            settings["files"] = {}
+        settings["files"][source.as_posix()] = int(source.stat().st_mtime)
 
 
 # Recursively iterate over all files in the source directory
@@ -39,6 +50,15 @@ for source_file in SOURCE.rglob(search):
     # Calculate the destination path of the source file
     dest_file = DEST / relative_path
 
-    mpremote_fs_cp(source_file, dest_file)
+    # if file is newer, copy it
+    source_modified = int(source_file.stat().st_mtime)
+    dest_modified = settings.get("files", {}).get(source_file.as_posix(), 0)
+    if source_modified > dest_modified:
+        mpremote_fs_cp(source_file, dest_file)
+    else:
+        print(f"Skipping {source_file}")
+
+with SETTINGS.open("w") as f:
+    yaml.dump(settings, f)
 
 print("Done.")
