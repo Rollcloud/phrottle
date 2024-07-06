e05948e94de4a1a49b51598b51cdf0c71193e518
rollcloud <rollcloud@gmail.com>
2024-07-06 23:24:47 +0100

    fix (scripts): introduce PID control to servo and make it quieter

--- src/scripts/servo.py
+++ src/scripts/servo.py
@@ -5,20 +5,53 @@ from SimplyRobotics import PIOServo  # type: ignore
 servo = PIOServo(18)
 servo.registerServo()
 
-sensor = ADC(1)
+sensor = ADC(0)
 
 
 def read_sensor():
-    return sensor.read_u16() >> 6
+    return sensor.read_u16() >> 7
 
+
+class PID:
+    """A simple PID controller."""
+
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+        self.error = 0
+        self.integral = 0
+        self.derivative = 0
+        self.last_error = 0
+
+    def update(self, error):
+        self.error = error
+        self.integral += self.error
+        self.derivative = self.error - self.last_error
+        self.last_error = self.error
+
+        return self.kp * self.error + self.ki * self.integral + self.kd * self.derivative
+
+
+LIMIT_PER_STEP = 2.0  # degrees per step
+controller = PID(0.15, 0.02, 0.05)
+y = None
 
 try:
     while True:
-        value = read_sensor() / 1024 * 180
-        position = min(max(180 - value, 0), 180)
+        value = read_sensor() / 256 * 180
+        position = 180 - value
 
-        print("value =", value, end="          \r")
-        servo.goToPosition(position)
-        utime.sleep(2)
+        if y is None:
+            y = position
+
+        error = position - y
+        y += min(max(controller.update(error), -LIMIT_PER_STEP), LIMIT_PER_STEP)
+        y = min(max(y, 0), 180)  # limit to 0-180 degrees
+
+        print("value =", value, "position =", position, "y =", y, end="          \r")
+        servo.goToPosition(y)
+        utime.sleep(0.01)
 finally:
     servo.deregisterServo()
