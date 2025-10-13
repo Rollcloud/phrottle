2a14adc17b77bb76402531dfd87d4f034878d126
rollcloud <rollcloud@gmail.com>
2025-10-13 23:05:34 +0100

    refactor: synchronise hardware and stately files across controllers

--- src/rp2-fire-and-ice/hardware.py
+++ src/rp2-fire-and-ice/hardware.py
@@ -67,39 +67,6 @@ class PWM_LED:
         self.pwm.duty_u16(65535)
 
 
-class TriColourLED:
-    """A RGB LED."""
-
-    OFF = (0, 0, 0)
-    RED = (1, 0, 0)
-    YELLOW = (1, 1, 0)
-    GREEN = (0, 1, 0)
-    BLUE = (0, 0, 1)
-    PURPLE = (1, 0, 1)
-    WHITE = (1, 1, 1)
-
-    def __init__(self, red_gpio, green_gpio, blue_gpio) -> None:
-        self.red_pin = Pin(red_gpio, Pin.OUT)
-        self.green_pin = Pin(green_gpio, Pin.OUT)
-        self.blue_pin = Pin(blue_gpio, Pin.OUT)
-
-        self.colour(self.OFF)
-
-    def colour(self, new_colour):
-        self.current_colour = new_colour
-        r, g, b = new_colour
-        # invert the values for common anode
-        self.red_pin.value(1 - r)
-        self.green_pin.value(1 - g)
-        self.blue_pin.value(1 - b)
-
-    def toggle(self, colour_1, colour_2):
-        if self.current_colour == colour_1:
-            self.colour(colour_2)
-        else:
-            self.colour(colour_1)
-
-
 class Indicator:
     """A visual element that can appear in different guises, coupled with advanced effects."""
 
